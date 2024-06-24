135fcecfd14b18c16c7c7e15226fa74dcc8bccb4
Eric Moore <ericmoore0709@gmail.com>
2024-06-24 01:11:48 -0700

    Fixed isPalindrome bug

--- recursion.js
+++ recursion.js
@@ -22,8 +22,8 @@ function everyOther(str) {
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
 function isPalindrome(str) {
-  if (str.length === 1) return true;
-  return (str[0] === str[str.length - 1] && isPalindrome(str.slice(1, str.length - 2)));
+  if (str.length <= 1) return true;
+  return (str[0] === str[str.length - 1] && isPalindrome(str.slice(1, str.length - 1)));
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
